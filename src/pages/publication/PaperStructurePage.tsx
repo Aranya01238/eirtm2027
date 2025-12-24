440c4062043266d774c65d327002c16c1fde479c
Aranya Rath <arjundutt01238@gmail.com>
2025-12-24 11:50:25 +0530

    Update PDF import path in PaperStructurePage

--- src/pages/publication/PaperStructurePage.tsx
+++ src/pages/publication/PaperStructurePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PaperStructurePdf from '@/assets/index_delayed_ui.pdf'; // Import the PDF file
+import PaperStructurePdf from '@/assets/ICEBM 2026-paper-format'; // Import the PDF file
 
 const PaperStructurePage = () => {
   return (
