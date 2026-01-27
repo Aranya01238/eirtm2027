01dbf619db21f5d6abfff44313ad9cea175291ae
ARANYA RATH <arjundutt01238@gmail.com>
2026-01-27 22:57:40 +0530

    FF

---
+++ src/config/logoSources.ts
@@ -0,0 +1,2 @@
+export const iemLogoUrl: string | null = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtZ2VlRmADbRE90Q4zCqWRRSDJFjIL0HjyFw&s";
+export const uemLogoUrl: string | null = "https://1000logos.net/wp-content/uploads/2023/03/UEM-Logo.png";
